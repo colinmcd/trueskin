713ff6486ed60739c93297bd12063995456ff50a
Aaron Smulktis <aaronsmulktis@gmail.com>
2015-11-14 17:12:41 -0500

    yes the loop was missing from page.php

--- page.php
+++ page.php
@@ -41,97 +41,9 @@ get_header();
 				// Include the page content template.
 				get_template_part( 'content', 'page' );
 
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-
 			// End the loop.
 			endwhile;
 			?>
-			
-			<div class="section regular-page" style="padding-top:61px;">
-				
-				<div class="container">
-					<div class="row">
-						<div class="col-xs-12">
-							<div class="height120"></div>
-
-							<div class="">
-								<div class="col-xs-12 border">
-									<div class="mission">
-										<div class="row">
-											<div class="col-xs-12  border-bottom">
-												<div class="mission-header">
-													<img src="<?php echo $image ?>" alt="">
-												</div>
-											</div>
-										</div>
-										<div class="row">
-											<div class="col-sm-6 col-xs-12 mission-mission border-right border-bottom">
-												<div class="padding">
-													<h3>
-														Mission:
-													</h3>
-													
-													<?php echo $mission ?>
-												</div>
-											</div>
-											<div class="col-sm-6 col-xs-12 border-bottom">
-												<div class="padding">
-													<h3>
-														WHAT WE DO:
-													</h3>
-												
-													<?php echo $what_we_do ?>
-												</div>
-											</div>
-										</div>
-										<div class="row">
-											<div class="col-xs-12 border-bottom">
-												<div class="padding">
-													<h3>
-														What we are
-													</h3>
-													<div class="mission-who-we-are">
-														<?php echo $who_we_are ?>
-													</div>
-												</div>
-											</div>
-										</div>
-										<div class="row">
-											<div class="col-xs-12 border-bottom">
-												<div class="padding">
-													<h3>
-														How you join
-													</h3>
-													<div class="mission-how-you-join">
-														<?php echo $how_you_join ?>
-													</div>
-												</div>
-											</div>
-										</div>
-										<div class="row">
-											<div class="col-xs-12">
-												<div class="mission-email">
-													<a href="mailto:yes@true.ink">yes@true.ink</a>
-												</div>
-											</div>
-										</div>
-									</div> <!-- mission -->
-
-								</div>
-							</div> <!-- row -->
-
-						</div> <!-- col-xs-12 -->
-					</div> <!-- row -->
-
-				</div> <!-- container -->
-				
-			</div> <!-- section -->
-			
-			
-			
 
 		</div>
 
